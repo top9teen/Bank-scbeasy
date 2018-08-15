8ed2eb639bba0f62587fef405e1ba5251826680e
top <kritkrai.rak@bru.ac.th>
2018-08-16 04:05:11 +0700

    Initial commit

---
+++ src/main/java/com/scbeasy/demo/BankScbeasyApplication.java
@@ -0,0 +1,12 @@
+package com.scbeasy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankScbeasyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankScbeasyApplication.class, args);
+	}
+}
---
+++ src/test/java/com/scbeasy/demo/BankScbeasyApplicationTests.java
@@ -0,0 +1,22 @@
+package com.scbeasy.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import org.springframework.test.context.web.WebAppConfiguration;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BankScbeasyApplicationTests.class)
+@WebAppConfiguration
+public class BankScbeasyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
